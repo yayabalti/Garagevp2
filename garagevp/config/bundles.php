c3c4b37cfb30490ba1ed39cad3f9c4fb3a0ab6cd
cissé <cissearnaud@gmail.com>
2024-12-27 18:19:37 +0100

    2eme config HttPS

--- garagevp/config/bundles.php
+++ garagevp/config/bundles.php
@@ -16,4 +16,6 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Kreait\Firebase\Symfony\Bundle\FirebaseBundle::class => ['all' => true],
 ];
